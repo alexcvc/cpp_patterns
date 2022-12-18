94e4f32412c8b194af71c99155a0844a721cd4a8
A.Sacharov <a.sacharov@gmx.de>
2022-12-18 21:41:53 +0100

    initial state of the repository after origin relocation

---
+++ examples/simple/main.cpp
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2022 Alexander Sacharov <a.sacharov@gmx.de>
+//               All rights reserved.
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+//
+
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+  std::cout << "Executable example" << std::endl;
+  return EXIT_SUCCESS;
+}
